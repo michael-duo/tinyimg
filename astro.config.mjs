9cf7e6145aa425333b531ec2812aa31af6d3fec3
Michael <michael@beraji.io>
2026-03-16 07:29:19 +0700

    refactor: update branding from CompressImg to TinyIMG across the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://compressimg.pages.dev',
+  site: 'https://tinyimg.michaelit.dev',
   integrations: [react(), sitemap()],
   output: 'static',
   vite: {
